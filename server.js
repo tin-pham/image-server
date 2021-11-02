6724489dde528a9aee51aad597f379749cbeb161
shin <heoconlinhat123@gmail.com>
2021-11-02 20:06:17 +0700

    Add CORS

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const jsonServer = require('json-server');
+var cors = require('cors');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json'); // <== Will be created later
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3200; // <== You can change the port
+
+server.use(
+  cors({
+    allowedHeaders: ['sessionId', 'Content-Type'],
+    exposedHeaders: ['sessionId'],
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+  })
+);
 
 server.use(middlewares);
 server.use(router);
